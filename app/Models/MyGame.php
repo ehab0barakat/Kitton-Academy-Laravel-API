e0629d361e0687517c6212fe41b89fc36d006797
ehab0barakat <mrehab9797@gmail.com>
2022-07-25 05:43:16 +0200

    good

---
+++ app/Models/MyGame.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MyGame extends Model
+{
+    use HasFactory;
+
+
+    protected $table = 'my_games';
+
+    protected $guarded = [];
+
+
+
+}
